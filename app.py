692733a226b8379c0e1b59281addbfaadc81338d
Nikhil Choudhary <nikhil.choudhary94@gmail.com>
2017-11-20 22:57:16 +0800

    basic layout

--- app.py
+++ app.py
@@ -7,10 +7,20 @@ def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>Hello passionate Raffles Banded Langur volunteer!</h1>
-    <p>It is currently {time}.</p>
+    <head>
+        <title>Raffles Banded Langur classifications</title>
+    </head>
+    <body>
+        <h1>Hello passionate Raffles Banded Langur volunteer!</h1>
+        <p>It is currently {time}.</p>
 
-    <img src="http://loremflickr.com/600/400">
+        <img src="http://loremflickr.com/600/400">
+        <img src="http://loremflickr.com/600/400">
+        <p>Are these two photos of the same langur? </p>
+        <input type="button" value="Not the same Langur">
+        <input type="button" value="I'm not certain">
+        <input type="button" value="Definitely the same Langur!">
+    </body>
     """.format(time=the_time)
 
 if __name__ == '__main__':
