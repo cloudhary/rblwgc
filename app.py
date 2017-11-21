4b38ed6491b46b0efa61dfeef90cb0c837ed2f34
Nikhil Choudhary <nikhil.choudhary94@gmail.com>
2017-11-21 16:51:43 +0800

    seperating files

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
-from flask import Flask
+import random
+from flask import Flask, redirect, url_for, render_template
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    return redirect(url_for('training'))
+
+@app.route('/training')
+def training():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <head>
-        <title>Raffles Banded Langur classifications</title>
-    </head>
-    <body>
-        <h1>Hello passionate Raffles Banded Langur volunteer!</h1>
-        <p>It is currently {time}.</p>
-
-        <img src="http://loremflickr.com/600/400">
-        <img src="http://loremflickr.com/600/400">
-        <p>Are these two photos of the same langur? </p>
-        <input type="button" value="Not the same Langur">
-        <input type="button" value="I'm not certain">
-        <input type="button" value="Definitely the same Langur!">
-    </body>
-    """.format(time=the_time)
+    image_ids=['a1', 'a2', 'b1', 'b2', 'c1', 'c2', 'd1', 'd2', 's1', 's2']
+    image_1 = random.choice(image_ids)
+    image_2 = random.choice(list(set(image_ids) - set(image_1)))
+    return render_template('training.html', time=the_time,
+        image_1=image_1, image_2=image_2, same=(image_1[0] == image_2[0]),
+        options=["Not at all!", "Not too sure..", "Definitely!"])
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
