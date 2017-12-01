f56184a7415b61ef1b9933ff5f19dcd57b22355b
Divya Sivasankaran <e0013178@u.nus.edu>
2017-12-01 17:52:27 +0800

    Adding user login functionality

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import random, os
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.bcrypt import Bcrypt
+
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key = 'my precious'
+
 url = os.environ.get('DATABASE_URL', None)
 if url: app.config['SQLALCHEMY_DATABASE_URI'] = url
 db = SQLAlchemy(app)
@@ -22,13 +27,70 @@ class Match(db.Model):
     def __repr__(self):
         return '<Image %r and %r have been classified as %r>' % (self.image_1, self.image_2, self.classification)
 
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+
+    def __repr__(self):
+        return '<name {}'.format(self.name)
+
 @app.route('/')
 def homepage():
     db.create_all()
-    return redirect(url_for('training'))
+    if session.get('logged_in') == True:
+        return redirect((url_for('training')))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        user = User.query.filter_by(name=request.form['username']).first()
+        if user is not None and bcrypt.check_password_hash(
+                user.password, request.form['password']):
+            session['logged_in'] = True
+            session['username'] = request.form['username']
+            return redirect(url_for('training'))
+        else:
+            error = 'Invalid Credentials. Please try again.'
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    session.pop('username', None)
+    flash("You were logged out successfully!")
+    return redirect(url_for('homepage'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = None
+    if request.method == 'POST':
+        db.session.add(User(request.form['username'],request.form['email'], request.form['password']))
+        db.session.commit()
+        session['logged_in'] = True
+        session['username'] = request.form['username']
+        return redirect(url_for('training'))
+    else:
+        error = 'Invalid Credentials. Please try again.'
+    return render_template('signup.html')
 
 @app.route('/training')
 def training():
+    if session.get('logged_in') != True:
+        return redirect(url_for('login'))
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
     image_ids=['a1', 'a2', 'b1', 'b2', 'c1', 'c2', 'd1', 'd2', 's1', 's2']
     image_1 = random.choice(image_ids)
@@ -39,6 +101,8 @@ def training():
 
 @app.route('/classify', methods=['GET', 'POST'])
 def classify():
+    if session.get('logged_in') != True:
+        return redirect(url_for('login'))
     image_ids=['a1', 'a2', 'b1', 'b2', 'c1', 'c2', 'd1', 'd2', 's1', 's2']
     image_1 = random.choice(image_ids)
     image_2 = random.choice([x for x in image_ids if x != image_1])
