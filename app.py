ec2a94bdd9747b8cef21c936305fe6b86b5e6d37
kianhean <kianhean@gmail.com>
2017-12-05 09:42:06 +0800

    simplify query

--- app.py
+++ app.py
@@ -120,10 +120,10 @@ def leaderboard():
     if session.get('logged_in') != True:
         return redirect(url_for('login'))
 
-    board = Match.query.join(User, User.id==Match.user_id).group_by(Match.user_id).add_column(func.count(Match.id)).add_column(Match.user_id).add_column(User.name).order_by(func.count(Match.id).desc()).limit(50)
+    board = Match.query.join(User, User.id==Match.user_id).group_by(Match.user_id).add_column(func.count(Match.id)).add_column(User.name).order_by(func.count(Match.id).desc()).limit(50)
     user = User.query.filter_by(name=session['username']).first()
     total_count = Match.query.filter_by(user_id=user.id).count()
-
+    print(board)
     return render_template('leaderboard.html', board=board,
                            total_count=total_count)
 
